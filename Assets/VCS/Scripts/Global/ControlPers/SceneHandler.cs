f1fecbb87e96c1eee1b738edb9803b4c57bc6220
ThreatLVL <ybiitsa4@mail.ru>
2025-04-22 15:14:55 +0700

    pull incident

--- Assets/VCS/Scripts/Global/ControlPers/SceneHandler.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class PersControl_SceneHandler : MonoBehaviour
-{
-    public const string SCENENAME_MENU = "Scene (Menu)";
-    public const string SCENENAME_MAIN = "Scene (Main)";
-}
