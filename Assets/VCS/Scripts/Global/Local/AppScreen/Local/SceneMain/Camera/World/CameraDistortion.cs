01a6254f88dc024f2c8ae4a9afab5af2218f78ef
Nepko147 <qqqeee53@gmail.com>
2025-07-03 15:16:03 +0700

    Distortion FIX

--- Assets/VCS/Scripts/Global/Local/AppScreen/Local/SceneMain/Camera/World/CameraDistortion.cs
+++ Assets/VCS/Scripts/Global/Local/AppScreen/Local/SceneMain/Camera/World/CameraDistortion.cs
@@ -6,65 +6,57 @@ public class AppScreen_Local_SceneMain_Camera_World_CameraDistortion : MonoBehav
 
     public bool Active { get; set; }
 
-    private float screenWidth;
-    private float screenHeight;
-
     private new Camera camera;
 
-    [SerializeField] private Material   distortion_material;
-    private const string                DISTORION_MATERIAL_U_ASPECT = "u_aspect";
-    private const string                DISTORION_MATERIAL_U_TEXNORMALMAP = "u_texNormalMap";
+    [SerializeField] private Material distortion_material;
+    private const string DISTORION_MATERIAL_U_ASPECT = "_MainNormalMap_aspect";
+    private const string DISTORION_MATERIAL_U_TEXNORMALMAP = "_MainNormalMap";
 
-    [SerializeField] private Material   normalMapMix_material;
-    private const string                NORMALMAPMIX_MATERIAL_OVERLAYTEX = "_OverlayTex";
-    private const string                NORMALMAPMIX_MATERIAL_OVERLAYTEXALPHA = "_OverlayTexApha";
-    private const string                NORMALMAPMIX_MATERIAL_OVERLAYTEXPOSX = "_OverlayTexPosX";
-    private const string                NORMALMAPMIX_MATERIAL_OVERLAYTEXPOSY = "_OverlayTexPosY";
-    private const string                NORMALMAPMIX_MATERIAL_OVERLAYTEXSCALE = "_OverlayTexScale";
-    private const string                NORMALMAPMIX_MATERIAL_ASPECTX = "_AspectX";
-    private const string                NORMALMAPMIX_MATERIAL_ASPECTY = "_AspectY";
+    private const string OVERLAYNORMALMAP = "_OverlayNormalMap";
+    private const string OVERLAYNORMALMAP_ALPHA = "_OverlayNormalMap_aplha";
+    private const string OVERLAYNORMALMAP_POS_X = "_OverlayNormalMap_pos_X";
+    private const string OVERLAYNORMALMAP_POS_Y = "_OverlayNormalMap_pos_Y";
+    private const string OVERLAYNORMALMAP_SCALE = "_OverlayNormalMap_Scale";
+    [SerializeField] private Sprite overlayNormalMap_sprite;
+    [SerializeField] private float overlayNormalMap_coinRush_visualDiameterInPixels = 120f;
+    private float overlayNormalMap_coinRush_scale = 0;
+    private const float OVERLAYNORMALMAP_COINRUSH_SCALE_MAX = 4f;
+    private const float OVERLAYNORMALMAP_COINRUSH_SCALE_STEP = OVERLAYNORMALMAP_COINRUSH_SCALE_MAX / OVERLAYNORMALMAP_COINRUSH_TIME_MAX;
+    private float overlayNormalMap_coinRush_time = 0;
+    private const float OVERLAYNORMALMAP_COINRUSH_TIME_MAX = 2f;
+    private float overlayNormalMap_coinRush_screenPos_x;
+    private float overlayNormalMap_coinRush_screenPos_y;
 
-    [SerializeField] private Texture2D  normalMapMix_material_normalMap_clear;
+    private const string SCREEN_WIDTH = "_Screen_widht";
+    private const string SCREEN_HEIGHT = "_Screen_height";
 
-    public bool                         NormalMapMix_Material_NormalMap_CoinRush_Active { get; private set; }
-    [SerializeField] private Sprite     normalMapMix_material_normalMap_coinRush_sprite;
-    private Texture2D                   normalMapMix_material_normalMap_coinRush_texture;
-    [SerializeField] private float      normalMapMix_material_normalMap_coinRush_visualDiameterInPixels = 120f;
-    private float                       normalMapMix_material_normalMap_coinRush_scale = 0;
-    private const float                 NORMALMAPMIX_MATERIAL_NORMALMAP_COINRUSH_SCALE_MAX = 4f;
-    private const float                 NORMALMAPMIX_MATERIAL_NORMALMAP_COINRUSH_SCALE_STEP = NORMALMAPMIX_MATERIAL_NORMALMAP_COINRUSH_SCALE_MAX / NORMALMAPMIX_MATERIAL_NORMALMAP_COINRUSH_TIME_MAX;
-    private float                       normalMapMix_material_normalMap_coinRush_time = 0;
-    private const float                 NORMALMAPMIX_MATERIAL_NORMALMAP_COINRUSH_TIME_MAX = 2f;
-    public Vector3                      NormalMapMix_Material_NormalMap_CoinRush_WorldPos { get; private set; }
-    private float                       normalMapMix_material_normalMap_coinRush_screenPos_x;
-    private float                       normalMapMix_material_normalMap_coinRush_screenPos_y;
+    [SerializeField] private Texture2D normalMap_clear;
+    [SerializeField] private Texture2D normalMap_gameOver;
+    private bool normalMap_gameOver_Active = false;
+    private bool normalMap_gameOver_updated = false;
 
-    [SerializeField] private Texture2D  normalMapMix_material_normalMap_gameOver;
-    private bool                        normalMapMix_material_normalMap_gameOver_Active = false;
-    private bool                        normalMapMix_material_normalMap_gameOver_updated = false;
+    public bool NormalMapMix_Material_NormalMap_CoinRush_Active { get; private set; }
 
-    private RenderTexture               normalMapMix_material_normalMap_output_renderTexture;
-    private Rect                        normalMapMix_material_normalMap_output_rect;
-    private Texture2D                   normalMapMix_material_normalMap_output_texture2d;
+    public Vector3 NormalMapMix_Material_NormalMap_CoinRush_WorldPos { get; private set; }
 
     public void CoinRush(Vector3 _position)
     {
         NormalMapMix_Material_NormalMap_CoinRush_Active = true;
         NormalMapMix_Material_NormalMap_CoinRush_WorldPos = _position;
         Vector2 _screenPosition = camera.WorldToScreenPoint(_position);
-        normalMapMix_material_normalMap_coinRush_screenPos_x = _screenPosition.x / screenWidth;
-        normalMapMix_material_normalMap_coinRush_screenPos_y = _screenPosition.y / screenHeight;
+        overlayNormalMap_coinRush_screenPos_x = _screenPosition.x / Screen.width;
+        overlayNormalMap_coinRush_screenPos_y = _screenPosition.y / Screen.height;
     }
 
     public float CoinRush_Distance_Get()
     {
-        var _visualDiameterInWorld = normalMapMix_material_normalMap_coinRush_visualDiameterInPixels / normalMapMix_material_normalMap_coinRush_sprite.pixelsPerUnit;
-        return (_visualDiameterInWorld * normalMapMix_material_normalMap_coinRush_scale);
+        var _visualDiameterInWorld = overlayNormalMap_coinRush_visualDiameterInPixels / overlayNormalMap_sprite.pixelsPerUnit;
+        return (_visualDiameterInWorld * overlayNormalMap_coinRush_scale);
     }
 
     public void GameOver()
     {
-        normalMapMix_material_normalMap_gameOver_Active = true;
+        normalMap_gameOver_Active = true;
     }
 
     private void Awake()
@@ -72,69 +64,65 @@ public class AppScreen_Local_SceneMain_Camera_World_CameraDistortion : MonoBehav
         SingleOnScene = this;
 
         Active = true;
-
-        screenWidth = Screen.width;
-        screenHeight = Screen.height;
+        NormalMapMix_Material_NormalMap_CoinRush_Active = false;
 
         camera = GetComponent<Camera>();
-
-        distortion_material.SetTexture(DISTORION_MATERIAL_U_TEXNORMALMAP, normalMapMix_material_normalMap_clear); //ѕомещаем пустую нормаль в шейдере искажени€
-
-        NormalMapMix_Material_NormalMap_CoinRush_Active = false;
-        var _distortion_material_aspect = (float)normalMapMix_material_normalMap_clear.height / normalMapMix_material_normalMap_clear.width;
-        distortion_material.SetFloat(DISTORION_MATERIAL_U_ASPECT, _distortion_material_aspect);
-        normalMapMix_material_normalMap_coinRush_texture = normalMapMix_material_normalMap_coinRush_sprite.texture;
-
-        normalMapMix_material_normalMap_output_renderTexture = new RenderTexture(normalMapMix_material_normalMap_clear.width, normalMapMix_material_normalMap_clear.height, 32); //ѕодготавливаем рендер-текстуру, в которой будем собирать нормаль дл€ шейдера искажени€ из пустой нормали и нормали искажени€
-        normalMapMix_material_normalMap_output_rect = new Rect(0, 0, normalMapMix_material_normalMap_output_renderTexture.width, normalMapMix_material_normalMap_output_renderTexture.height); //GETRECT!
-        normalMapMix_material_normalMap_output_texture2d = new Texture2D(normalMapMix_material_normalMap_clear.width, normalMapMix_material_normalMap_clear.height, TextureFormat.RGBA32, false); //ѕодготавливаем 2D-текстуру, в которую будем помещать нормаль дл€ шейдера искажени€ собранную в рендер-текстуре
     }
 
-    private void OnRenderImage(RenderTexture _source, RenderTexture _destination)
+    private void Start()
+    {
+        distortion_material.SetTexture(DISTORION_MATERIAL_U_TEXNORMALMAP, normalMap_clear); //ѕомещаем пустую нормаль в шейдере искажени€
+        distortion_material.SetTexture(OVERLAYNORMALMAP, overlayNormalMap_sprite.texture);
+        var _distortion_material_aspect = (float)normalMap_clear.height / normalMap_clear.width;
+        distortion_material.SetFloat(DISTORION_MATERIAL_U_ASPECT, _distortion_material_aspect);
+        distortion_material.SetFloat(OVERLAYNORMALMAP_SCALE, overlayNormalMap_coinRush_scale);
+    }
+
+    private void Update()
     {
         if (Active)
         {
-            if (normalMapMix_material_normalMap_gameOver_Active)
+            if (normalMap_gameOver_Active)
             {
-                if (!normalMapMix_material_normalMap_gameOver_updated)
+                if (!normalMap_gameOver_updated)
                 {
-                    distortion_material.SetTexture(DISTORION_MATERIAL_U_TEXNORMALMAP, normalMapMix_material_normalMap_gameOver); //ѕомещаем нормаль искажени€ Game Over'а в шейдер искажени€
-                    normalMapMix_material_normalMap_gameOver_updated = true;
+                    distortion_material.SetTexture(DISTORION_MATERIAL_U_TEXNORMALMAP, normalMap_gameOver); //ѕомещаем нормаль искажени€ Game Over'а в шейдер искажени€
+                    normalMap_gameOver_updated = true;
                 }
             }
             else
             {
                 if (NormalMapMix_Material_NormalMap_CoinRush_Active)
                 {
-                    normalMapMix_material.SetTexture(NORMALMAPMIX_MATERIAL_OVERLAYTEX, normalMapMix_material_normalMap_coinRush_texture);
-                    float _alpha = 1f - normalMapMix_material_normalMap_coinRush_scale / NORMALMAPMIX_MATERIAL_NORMALMAP_COINRUSH_SCALE_MAX;
+                    float _alpha = 1f - overlayNormalMap_coinRush_scale / OVERLAYNORMALMAP_COINRUSH_SCALE_MAX;
                     _alpha = Mathf.Clamp(_alpha, 0, 1f);
-                    normalMapMix_material.SetFloat(NORMALMAPMIX_MATERIAL_OVERLAYTEXALPHA, _alpha);
-                    normalMapMix_material.SetFloat(NORMALMAPMIX_MATERIAL_OVERLAYTEXPOSX, normalMapMix_material_normalMap_coinRush_screenPos_x);
-                    normalMapMix_material.SetFloat(NORMALMAPMIX_MATERIAL_OVERLAYTEXPOSY, normalMapMix_material_normalMap_coinRush_screenPos_y);
-                    normalMapMix_material.SetFloat(NORMALMAPMIX_MATERIAL_OVERLAYTEXSCALE, normalMapMix_material_normalMap_coinRush_scale);
-                    normalMapMix_material.SetFloat(NORMALMAPMIX_MATERIAL_ASPECTX, screenWidth);
-                    normalMapMix_material.SetFloat(NORMALMAPMIX_MATERIAL_ASPECTY, screenHeight);
-                    Graphics.Blit(normalMapMix_material_normalMap_clear, normalMapMix_material_normalMap_output_renderTexture, normalMapMix_material); //—овмещаем пустую нормаль с нормалью искажени€ Coin Rush'а через шейдер совмещени€ нормалей. «атем, помещаем результат в подготовленную рендер-тектуру 
 
-                    normalMapMix_material_normalMap_output_texture2d.ReadPixels(normalMapMix_material_normalMap_output_rect, 0, 0, false); //ѕомещаем пиксели с подготовленной рендер-текстуры в поле Colors подготовленной 2D-текстуры
-                    normalMapMix_material_normalMap_output_texture2d.Apply(); //ќтрисовываем на подготовленной 2D-текстуре, помещЄнные в еЄ поле Colors пиксели, с получением нормали дл€ шейдера искажени€, котора€ совмещает в себе пустую нормаль и нормаль искажени€ Coin Rush'а
+                    distortion_material.SetFloat(OVERLAYNORMALMAP_ALPHA, _alpha);
+                    distortion_material.SetFloat(OVERLAYNORMALMAP_POS_X, overlayNormalMap_coinRush_screenPos_x);
+                    distortion_material.SetFloat(OVERLAYNORMALMAP_POS_Y, overlayNormalMap_coinRush_screenPos_y);
+                    distortion_material.SetFloat(OVERLAYNORMALMAP_SCALE, overlayNormalMap_coinRush_scale);
+                    distortion_material.SetFloat(SCREEN_WIDTH, Screen.width);
+                    distortion_material.SetFloat(SCREEN_HEIGHT, Screen.height);
 
-                    distortion_material.SetTexture(DISTORION_MATERIAL_U_TEXNORMALMAP, normalMapMix_material_normalMap_output_texture2d); //ѕомещаем нормаль из подготовленной 2D-текстуры в шейдер искажени€
+                    overlayNormalMap_coinRush_scale += OVERLAYNORMALMAP_COINRUSH_SCALE_STEP * Time.deltaTime;
+                    overlayNormalMap_coinRush_time += Time.deltaTime;
 
-                    normalMapMix_material_normalMap_coinRush_scale += NORMALMAPMIX_MATERIAL_NORMALMAP_COINRUSH_SCALE_STEP * Time.deltaTime;
-                    normalMapMix_material_normalMap_coinRush_time += Time.deltaTime;
-
-                    if (normalMapMix_material_normalMap_coinRush_time >= NORMALMAPMIX_MATERIAL_NORMALMAP_COINRUSH_TIME_MAX)
+                    if (overlayNormalMap_coinRush_time >= OVERLAYNORMALMAP_COINRUSH_TIME_MAX)
                     {
-                        normalMapMix_material_normalMap_coinRush_scale = 0;
-                        normalMapMix_material_normalMap_coinRush_time = 0;
+                        _alpha = 0;
+                        distortion_material.SetFloat(OVERLAYNORMALMAP_ALPHA, _alpha);
+                        overlayNormalMap_coinRush_scale = 0;
+                        distortion_material.SetFloat(OVERLAYNORMALMAP_SCALE, overlayNormalMap_coinRush_scale);
+                        overlayNormalMap_coinRush_time = 0;
                         NormalMapMix_Material_NormalMap_CoinRush_Active = false;
                     }
                 }
             }
         }
+    }
 
+    private void OnRenderImage(RenderTexture _source, RenderTexture _destination)
+    {
         Graphics.Blit(_source, _destination, distortion_material); //»скажаем рендер-текстуру с камеры текущего GameObject'а через шейдер искажени€. «атем помещаем результат в рендер-текстуру камеры текущего GameObject'а
     }
 }
--- Assets/VCS/Shaders/Distortion.shader
+++ Assets/VCS/Shaders/Distortion.shader
@@ -2,9 +2,16 @@
 {
     Properties
     {
-        _MainTex ("Texture", 2D) = "white" {}
-        u_texNormalMap ("u_texNormalMap", 2D) = "bump" {}
-        u_strength ("u_strength", Range (0, 1)) = 0.04
+        _MainTex ("Main Texture", 2D) = "white" {}
+        _MainNormalMap ("Main Normal Map", 2D) = "bump" {}
+
+        _OverlayNormalMap ("Overlay Norma Map", 2D) = "bump" {}        
+        _OverlayNormalMap_aplha("Overlay Norma Map Alpha", Range (0, 1)) = 0.04
+        _OverlayNormalMap_pos_X ("Overlay Norma Map Position X", Range (-1, 1)) = 0.04
+        _OverlayNormalMap_pos_Y ("Overlay Norma Map Position Y", Range (-1, 1)) = 0.04
+        _OverlayNormalMap_Scale ("Overlay Texture Scale", Range (0, 10)) = 0.04
+
+        _Strength ("_Strength", Range (0, 1)) = 0.1
     }
 
     Subshader
@@ -26,34 +33,65 @@
 
             struct v2f {
                 float2 uv : TEXCOORD0;
+                float2 uv_texNormalMap : TEXCOORD1;
+                float2 uv_OverlayNormalMap : TEXCOORD2;
                 float4 vertex : SV_POSITION;
             };
 
             float4 _MainTex_ST;
 
             sampler2D _MainTex;
-            sampler2D u_texNormalMap;
-            fixed u_strength;
-            fixed u_aspect;            
 
-            float2 correction;
+            float _Screen_widht;
+            float _Screen_height;
+
+            sampler2D   _MainNormalMap;
+            fixed       _MainNormalMap_aspect;
+
+            sampler2D   _OverlayNormalMap;
+            float       _OverlayNormalMap_aplha;
+            fixed       _OverlayNormalMap_pos_X;
+            fixed       _OverlayNormalMap_pos_Y;
+            fixed       _OverlayNormalMap_Scale;
+
+            fixed _Strength;            
+
+            float2 correction;  
 
             v2f vert(appdata_t v)
             {
                 v2f o;
                 o.vertex = UnityObjectToClipPos(v.vertex);
                 o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                o.uv_texNormalMap = v.uv;
+                o.uv_OverlayNormalMap.x = ((v.uv.x - _OverlayNormalMap_pos_X + ((_Screen_height / _Screen_widht) / 2) * _OverlayNormalMap_Scale) / _OverlayNormalMap_Scale * (_Screen_widht / _Screen_height));
+                o.uv_OverlayNormalMap.y = ((v.uv.y - _OverlayNormalMap_pos_Y + 0.5 * _OverlayNormalMap_Scale) / _OverlayNormalMap_Scale);
                 return o;
             }
 
             fixed4 frag(v2f i) : SV_Target
             {
-                // Получаем текущий цвет пикселя
-                float2 offset = (tex2D(u_texNormalMap, i.uv).rg - 0.5) * 2.0 * u_strength;
-                //offset.x /= u_aspect;
+                fixed4 col = tex2D(_MainNormalMap, i.uv_texNormalMap);
+                fixed4 overlayCol = tex2D(_OverlayNormalMap, i.uv_OverlayNormalMap);
                 
-                correction.x = u_strength * u_aspect + (u_strength * 0.014); // Автоматическая крректировка положения изображения вместо "костыликов"
-                correction.y = u_strength * u_aspect + (u_strength * 0.006); // Автоматическая крректировка положения изображения вместо "костыликов"
+
+                if (!(overlayCol.a == 0 
+                    || i.uv_OverlayNormalMap.x > 1
+                    || i.uv_OverlayNormalMap.y > 1
+                    || i.uv_OverlayNormalMap.y < 0
+                    || i.uv_OverlayNormalMap.x < 0))
+                {
+                    overlayCol.a = _OverlayNormalMap_aplha;
+                    fixed4 newColor = (col * (1 - overlayCol.a)) + overlayCol * overlayCol.a;                     
+                    newColor.a = col.a; 
+                    col = newColor;
+                }
+
+                // Получаем текущий цвет пикселя
+                float2 offset = (col.rg - 0.5) * 2.0 * _Strength;
+                
+                correction.x = _Strength * _MainNormalMap_aspect + (_Strength * 0.014); // Автоматическая крректировка положения изображения вместо "костыликов"
+                correction.y = _Strength * _MainNormalMap_aspect + (_Strength * 0.006); // Автоматическая крректировка положения изображения вместо "костыликов"
                 
                 fixed4 color = tex2D(_MainTex, i.uv + offset + correction);
                 return color; // Комбинируем оригинальный цвет с размытым
